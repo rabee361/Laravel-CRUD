8040ec6dd4a8950332dd32d034c2bd67f1345ca3
rabee361 <rha60540@gmail.com>
2025-05-27 22:00:05 +0300

    fixed api endpoints not showing

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -8,6 +8,6 @@ class PostsController extends Controller
 {
     public function index()
     {
-        return ['hello' => 'world'];
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\Api\PostsController;
 use Illuminate\Support\Facades\Route;
 
-// Make sure this is correctly defined
-Route::apiResource('posts', PostsController::class);
-
 // For debugging, add a test route
 Route::get('test', function() {
     return response()->json(['message' => 'API is working']);
